540b79a58180a467937898c24ecf05a8e867a36d
elvis <gicharuelvis@gmail.com>
2022-08-01 09:32:11 +0300

    commit tree a764a97338e20e6dc5e557d592e8bad4f5a03d22

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}// }saturdayFun('bathe my dog')
+
+function mondayWork(work="go to the office"){
+    return `This Monday, I will ${work}.`;
+}
+
+let  wrapAdjective=function(star='*'){
+    return function innerwrapAdjective(wrap="special"){
+        return `You are ${star}${wrap}${star}!`;
+    }
+    
+}
+
